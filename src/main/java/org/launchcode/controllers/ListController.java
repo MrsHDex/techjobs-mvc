731e83ee71b79ba807e017cf6f9891a129db2e33
MrsHDex <thompsonac542@gmail.com>
2019-04-17 10:16:29 -0400

    Tech Jobs Final Draft
    Final save before the project is checked for accuracy.

--- src/main/java/org/launchcode/controllers/ListController.java
+++ src/main/java/org/launchcode/controllers/ListController.java
@@ -18,13 +18,14 @@ public class ListController {
 
     static HashMap<String, String> columnChoices = new HashMap<>();
 
-    public ListController () {
+    public ListController() {
         columnChoices.put("core competency", "Skill");
         columnChoices.put("employer", "Employer");
         columnChoices.put("location", "Location");
         columnChoices.put("position type", "Position Type");
         columnChoices.put("all", "All");
     }
+
 
     @RequestMapping(value = "")
     public String list(Model model) {
@@ -35,29 +36,29 @@ public class ListController {
     }
 
     @RequestMapping(value = "values")
-    public String listColumnValues(Model model, @RequestParam String column) {
+    public String listColumnValues(Model model, @RequestParam String column){
 
-        if (column.equals("all")) {
+        if (column.equals("all")){
             ArrayList<HashMap<String, String>> jobs = JobData.findAll();
-            model.addAttribute("title", "All Jobs");
+            model.addAttribute("title", "All Jobs ");
             model.addAttribute("jobs", jobs);
             return "list-jobs";
-        } else {
+        }else {
             ArrayList<String> items = JobData.findAll(column);
-            model.addAttribute("title", "All " + columnChoices.get(column) + " Values");
+            model.addAttribute("title", "All "
+                    + columnChoices.get(column) + " Values");
             model.addAttribute("column", column);
             model.addAttribute("items", items);
             return "list-column";
         }
-
     }
 
     @RequestMapping(value = "jobs")
-    public String listJobsByColumnAndValue(Model model,
-            @RequestParam String column, @RequestParam String value) {
-
+    public String listJobsByColumnAndValue(Model model, @RequestParam String column,
+                                           @RequestParam String value){
         ArrayList<HashMap<String, String>> jobs = JobData.findByColumnAndValue(column, value);
-        model.addAttribute("title", "Jobs with " + columnChoices.get(column) + ": " + value);
+        model.addAttribute("title", "Jobs with "
+                + columnChoices.get(column) + ": " + value);
         model.addAttribute("jobs", jobs);
 
         return "list-jobs";
--- src/main/java/org/launchcode/controllers/SearchController.java
+++ src/main/java/org/launchcode/controllers/SearchController.java
@@ -4,10 +4,15 @@ import org.launchcode.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Map;
+
+import static org.launchcode.controllers.ListController.columnChoices;
 
 /**
  * Created by LaunchCode
@@ -18,10 +23,29 @@ public class SearchController {
 
     @RequestMapping(value = "")
     public String search(Model model) {
-        model.addAttribute("columns", ListController.columnChoices);
+        model.addAttribute("columns", columnChoices);
         return "search";
     }
 
     // TODO #1 - Create handler to process search request and display results
+    @RequestMapping(value = "results")
+    public String search(Model model, String searchType, String searchTerm){
+        //search parameters listed by corresponding form field
+        model.addAttribute("columns", columnChoices);
+        if (searchType.equals("all")){ //search all in search section
+            ArrayList<HashMap<String, String>> searchResults = JobData.findByValue(searchTerm);
+            model.addAttribute("searchResults", searchResults);
+            return "search";
+        }else { //search by type column and keyword (searchTerm)
+            ArrayList<HashMap<String, String>> searchResults = JobData.findByColumnAndValue(searchType, searchTerm);
 
+            model.addAttribute("searchResults", searchResults);
+
+            return "search";
+        }
+    }
 }
+
+
+
+
